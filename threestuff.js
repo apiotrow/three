f0dcc15ca65a1e76c8fd7cc1231a8071eb60d5e2
Aaron Piotrowski <aaron.p.piotrowski@gmail.com>
2017-01-12 05:01:06 -0800

    lathing with dots

--- threestuff.js
+++ threestuff.js
@@ -3,10 +3,10 @@ module.exports = function(){
 	function go(){
 		var grammars = {
 	 		j: {
-	 			a: "bcb",
-				b: "cbc",
-				c: "dcd",
-				d: "ada",
+	 			a: "ba",
+				b: "cb",
+				c: "dc",
+				d: "ad",
 				// e: "ea"
 	 		},
 	 		six: {
@@ -76,7 +76,7 @@ module.exports = function(){
 			f: [0, 0, -1],
 		}
 
-	 	var Lstring = makeLString("a", 2, grammars.j);
+	 	var Lstring = makeLString("a", 4, grammars.j);
 	 	Lstring = "c";
 		console.log(Lstring);
 
@@ -93,7 +93,7 @@ module.exports = function(){
 
 		var Lmatrix = [];
 		for(var i = 0; i < Lstring.length; i++){
-			var L = makeLString(Lstring.charAt(i), 5, grammars.j);
+			var L = makeLString(Lstring.charAt(i), 8, grammars.j);
 			var arrayL = [];
 			for(var j = 0; j < L.length; j++){
 				arrayL.push(L.charAt(j));
@@ -101,7 +101,6 @@ module.exports = function(){
 			Lmatrix.push(arrayL);
 			// console.log(L);
 		}
-		console.log(Lmatrix);
 
 		g = new THREE.Group();
 		var vec = [0, 0, 0];
@@ -118,26 +117,46 @@ module.exports = function(){
 			}
 		}
 
-		scene.add(g);
-		g.rotation.x = 350 * Math.PI / 180;
-		// var vector = new THREE.Vector3();
-		// g.children[20].localToWorld( vector );
-		// console.log(vector);
-
-
-		// for(var i = 0; i < Lstring.length; i += 3){
-		// 	var Lchar1 = Lstring.charAt(i);
-		// 	var Lchar2 = Lstring.charAt(i + 1);
-		// 	var Lchar3 = Lstring.charAt(i + 2);
-		// 	var Lval1 = wordVals[Lchar1];
-		// 	var Lval2 = wordVals[Lchar2];
-		// 	var Lval3 = wordVals[Lchar3];
-		// 	vec[0] += Lval1;
-		// 	vec[1] += Lval2;
-		// 	vec[2] += Lval3;
-		// 	// console.log(vec);
-		// 	addCube(vec[0], vec[1], vec[2]);
+		// g = new THREE.Group();
+		// var dotGeometry = new THREE.Geometry();
+		// var vec = [0, 0, 0];
+		// for(var i = 0; i < Lmatrix.length; i++){
+		// 	vec = [0, 0, 0];
+		// 	for(var j = 0; j < Lmatrix[i].length; j++){
+		// 		// console.log(Lmatrix[i]);
+		// 		var LMatrixChar = Lmatrix[i][j];
+		// 		vec[0] += vecChangeStrips[LMatrixChar][0];
+		// 		vec[1] += vecChangeStrips[LMatrixChar][1];
+		// 		vec[2] = i;
+		// 		dotGeometry.vertices.push(new THREE.Vector3(vec[0], vec[1], vec[2]));
+		// 		var dotMaterial = new THREE.PointsMaterial( { size: 1, sizeAttenuation: false } );
+		// 		var dot = new THREE.Points( dotGeometry, dotMaterial );
+		// 		g.add(dot);
+		// 	}
 		// }
+		// scene.add(g);
+		// console.log(g)
+
+		var newGroup = new THREE.Group();
+		var dotGeometry = new THREE.Geometry();
+		for(var i = 0; i < 360; i += 45){
+			// g.rotation.x = i * Math.PI / 180;
+			// g.rotation.z = i * Math.PI / 180;
+			g.rotation.y = i * Math.PI / 180;
+			g.updateMatrixWorld();
+			for(var j = 0; j < g.children.length; j++){
+				var vector = new THREE.Vector3();
+				vector.setFromMatrixPosition( g.children[j].matrixWorld );
+				dotGeometry.vertices.push(new THREE.Vector3(vector.x, vector.y, vector.z));
+				var dotMaterial = new THREE.PointsMaterial( { size: 1, sizeAttenuation: false } );
+				var dot = new THREE.Points( dotGeometry, dotMaterial );
+				newGroup.add(dot);
+			}
+		}
+		console.log(newGroup)
+		scene.add(newGroup);
+
+		
 	}
 
 
@@ -181,7 +200,6 @@ module.exports = function(){
 
  	var g;
 	go();
-	console.log(g.children[5]);
 	
 	var render = function () {
 		requestAnimationFrame(render);
@@ -197,7 +215,7 @@ module.exports = function(){
 	render();
 
 	var angle = 0;
-	var camDistance = 30; 
+	var camDistance = 90; 
 	function rotateCamera(){
 		var timer = Date.now() * 0.01;
 		camera.position.x = camDistance * Math.cos( angle );  
